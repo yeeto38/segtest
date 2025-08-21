a3cde7561bfdce35cf6a2d59660d6e7b282a7a72
huan2183 <huan2183@purdue.edu>
2025-08-20 23:57:08 -0700

    localparam sel0

--- SevenSeg.v
+++ SevenSeg.v
@@ -8,19 +8,23 @@ module SevenSeg #(
 	 input [8*NUMCELLS - 1:0] cellvalin,
 	 output [11:0] sig
 	);
-	reg [NUMCELLS-1:0] sel = ~(1 << (NUMCELLS -1));
+	localparam [NUMCELLS-1:0] sel0={1'b0,{(NUMCELLS-1){1'b1}}};
+	reg [NUMCELLS-1:0] sel = sel0;//~(1 << (NUMCELLS -1));
 	reg [8*NUMCELLS -1:0] r_cellvalin = 0;
 	reg [9:0] count=0;
+	wire [3:0] test = sel0;//(~(1 << (NUMCELLS - 1)));
 	// reg [7:0] tog=0;
-	
+	wire testt = sel == sel0;//(~(1 << (NUMCELLS - 1)));
+
 	always @(posedge clock) begin
 		count <= count + 1;
-		end
+	end
 	always @(posedge count[9]) begin
 			// if (tog == 8'b10000000) begin
 				sel <= {sel[NUMCELLS - 2:0], sel[NUMCELLS - 1]};
-				r_cellvalin <= sel == (~(1 << (NUMCELLS - 1))) ? cellvalin : 
-				{r_cellvalin[8*(NUMCELLS-1) - 1:0], r_cellvalin[8*NUMCELLS - 1:8*(NUMCELLS - 1)]};			
+				r_cellvalin <= sel == sel0 ? cellvalin : 
+		{r_cellvalin[8*(NUMCELLS-1) - 1:0], r_cellvalin[8*NUMCELLS - 1:8*(NUMCELLS - 1)]};			
+
 			// end
 			// tog <= tog + 1;
 	end
