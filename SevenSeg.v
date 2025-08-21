54329fb98f82962e0ad6442922f4341ab7905591
huan2183 <huan2183@purdue.edu>
2025-08-20 22:42:50 -0700

    fixing sevenseg.v and other things

--- SevenSeg.v
+++ SevenSeg.v
@@ -8,23 +8,24 @@ module SevenSeg #(
 	 input [8*NUMCELLS - 1:0] cellvalin,
 	 output [11:0] sig
 	);
-	reg [NUMCELLS-1:0] sel = 1 << (NUMCELLS -1);
+	reg [NUMCELLS-1:0] sel = ~(1 << (NUMCELLS -1));
 	reg [8*NUMCELLS -1:0] r_cellvalin = 0;
 	reg [9:0] count=0;
-	reg tog=0;
+	// reg [7:0] tog=0;
 	
 	always @(posedge clock) begin
 		count <= count + 1;
 		end
 	always @(posedge count[9]) begin
-			r_cellvalin <= sel == (1 << (NUMCELLS - 1)) ? 
-			cellvalin : 
-			{r_cellvalin[8*(NUMCELLS-1) - 1:0], r_cellvalin[8*NUMCELLS - 1:8*(NUMCELLS - 1)]};			
-			if (tog) begin
+			// if (tog == 8'b10000000) begin
 				sel <= {sel[NUMCELLS - 2:0], sel[NUMCELLS - 1]};
-			end
-			tog <= ~tog;
+				r_cellvalin <= sel == (~(1 << (NUMCELLS - 1))) ? cellvalin : 
+				{r_cellvalin[8*(NUMCELLS-1) - 1:0], r_cellvalin[8*NUMCELLS - 1:8*(NUMCELLS - 1)]};			
+			// end
+			// tog <= tog + 1;
 	end
-	assign sig = tog ? {sel,r_cellvalin[8*NUMCELLS - 1:8*(NUMCELLS - 1)]} : 
-				 {4'b0,r_cellvalin[8*NUMCELLS - 1:8*(NUMCELLS - 1)]};
+	assign sig =
+	//  tog == 8'b10000000 ? 
+	{sel,r_cellvalin[8*NUMCELLS - 1:8*(NUMCELLS - 1)]};
+	//  : {4'b0,r_cellvalin[8*NUMCELLS - 1:8*(NUMCELLS - 1)]};
 endmodule
--- sevensegin.v
+++ sevensegin.v
@@ -7,7 +7,7 @@ module sevensegin #(
 	 output [11:0] seg 
 	);
 wire [8 * NUMCELLS - 1:0] decodedtimer;
-reg [4 * NUMCELLS - 1:0] toutsig;
+wire [4 * NUMCELLS - 1:0] toutsig;
 wire [11:0] sig;
 Timer t (.rst(rst), .clock(clock), .elapsed(toutsig));
 TimerDecode timeout (.in(toutsig), .out(decodedtimer));
